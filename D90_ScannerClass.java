b9cf347e2f1303ebd3d7e096f95f69e51c2c0d4e
sudeer9 <43990469+sudeer9@users.noreply.github.com>
2022-06-24 21:08:42 +0530

    Add files via upload

---
+++ D90_ScannerClass.java
@@ -0,0 +1,21 @@
+package com.BasentTechnologies;
+
+import java.util.Scanner;
+
+public class D90_ScannerClass {
+	//Example to get input from console
+	//Example of the Java Scanner class which reads the int, string and double value as an input
+	
+		public static void main(String[] args) 
+		{
+			Scanner sc=new Scanner(System.in);
+			System.out.println("enter phone number");
+			sc.nextInt();
+			System.out.println("Entered OTP");
+			sc.next();
+			System.out.println("Hello");
+			
+		
+		}
+
+	}
---
+++ D91_Exceptions.java
@@ -0,0 +1,27 @@
+package com.BasentTechnologies;
+
+public class D91_Exceptions 
+{
+		
+	public static void main(String[] args) 
+	{
+			
+		//There are 5 keywords which are used in handling exceptions in Java
+		//try,catch,finally,throw and throws
+
+			int i=10;
+			int j=0;
+			try
+			{
+			int k=i/j;//this will throw an exception bcoz we can't divide a number by zero
+			System.out.println(k);
+			}
+			catch(Exception e)
+			{
+				System.out.println("cannot divide by zero");
+				System.out.println(e);
+			}
+			
+		}
+
+	}
